fa64252418e337c026333e13bead403d8214f5cd
Roy <rdg01834@gmail.com>
2020-12-05 12:15:53 +0600

    aded minimum functionality to complite Add Todo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  const [todos, setTodos] = useState(['todo state 1', 'todo state 2']);
+  const [input, setInput] = useState('');
+
+  const addTodo = (event) => {
+    //addTodo will hit when we click the button
+      //this will stop whole page refreshing
+    event.preventDefault();
+    setTodos([...todos, input]);
+    setInput(''); //clear the input after hit add Todo
+     
+  }
   return (
     <div className="App">
-      <h1>TODO</h1>
+      <form>
+        <input value = {input} 
+               onChange = {event => setInput(event.target.value)}/> 
+               {/* onChange capture what we are typing and we call an event to setInput value to show it on input field*/}
+        <button 
+          type = "submit"
+          onClick = {addTodo}>
+            Add Todo
+        </button>
+
+        <ul>
+          {
+            todos.map(todo => (
+            <li>{todo}</li>
+            ))
+          }
+        </ul>
+      </form>     
     </div>
   );
 }
