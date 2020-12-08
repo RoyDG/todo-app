91c7213b95ee70c1416fcdbf2ff95599b021c154
Roy <rdg01834@gmail.com>
2020-12-09 02:23:58 +0600

    css aded

--- src/Todo.js
+++ src/Todo.js
@@ -7,22 +7,28 @@ import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
 
 function Todo(props) {
     return (
-            <List className = "todo__list">
-                <ListItem>
-                <ListItemAvatar>
-                </ListItemAvatar>  
-                    <ListItemText
-                        className = "listText"
-                        primary = "Task --"
-                        secondary = {props.todo.todo} 
-                    />
-                </ListItem>
-                <DeleteOutlineIcon
-                    className= "removeIcon"
-                    onClick = {event => 
+            <div className = "todo">
+                <List className = "list">
+                    <div>                    
+                        <ListItem>
+                        <ListItemAvatar>
+                        </ListItemAvatar>  
+                        <ListItemText
+                            className = "listText"
+                            primary = "Task --"
+                            secondary = {props.todo.todo} 
+                        />
+                        </ListItem>
+                    </div>
+            
+                    <div className = "removeIcon">
+                        <DeleteOutlineIcon 
+                            onClick = {event => 
                         db.collection('todos').doc(props.todo.id).delete()
-                    }/>              
-            </List>
+                    }/> 
+                    </div>             
+                </List>
+            </div>
     )
 }
 
